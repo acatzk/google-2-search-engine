27b589258d8c78c8018f81fdf06a8881df2e669f
Joshua Galit <joshuaimalay@gmail.com>
2021-04-21 21:07:29 +0800

    init

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Google Search</title>
+      </Head>
+      <div>
+        <h1 className="text-2xl font-semibold text-indigo-600">Hello</h1>
+      </div>
+    </>
+  )
+}
