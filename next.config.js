2a3b30b624941ceacd8106b3f4c86f08ba70f206
Joshua Galit <joshuaimalay@gmail.com>
2021-04-21 23:16:06 +0800

    wip: google search homepage ui

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   future: {
     webpack5: true,
+  },
+  images: {
+    domains: ['www.google.co.uk']
   }
 }
--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/jit': {},
     autoprefixer: {},
-  },
-}
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  }
 }
